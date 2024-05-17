b1cd56a429dd7cb5903896bbb68aa21ca81471a8
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-17 20:01:52 +0200

    Added basics routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-
+  {
+    path: 'login', // part of url
+    component: LoginComponent, //component that we showing on this path
+  }, {
+    path: 'about',
+    component: AboutComponent,
+  }
 ];
 
 @NgModule({
   imports: [
-
+    //This function for setup router 
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
