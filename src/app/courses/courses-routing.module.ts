954473b0b0d575eb703ad450ae47f7131c1d5dad
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-18 00:21:41 +0200

    Added main app-routing with home page, page 404, and lazy loading module

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-
+  {
+    //this rout will be showing with address http:localhost:4200/courses
+    path: "",
+    component: HomeComponent,
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
   providers: [
