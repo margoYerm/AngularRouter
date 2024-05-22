310e198f869e0e0b19995e3a5d58aff250931d3c
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-22 23:48:12 +0200

    Using query params from the url; passing this params like attribute to another component

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -18,6 +18,14 @@ export class CourseComponent implements OnInit {
         //snapshot contains all information about current route
         //data contains all date for this component == resolve: {course: courseResolver}
         this. course = this.activatedRoute.snapshot.data["course"];
+
+        //http://localhost:4200/courses/angular-router-course?couponCode=NEW_YEAR
+        //we want show couponCode... if it's available
+        // for grab query params in the url once when component is init 
+        this.couponCode = this.activatedRoute.snapshot.queryParamMap.get('couponCode');
+
+        //for grab query params in the url like Observable if it's changing
+        //this.activatedRoute.queryParams 
     }
 
     confirmExit() {        
@@ -25,6 +33,8 @@ export class CourseComponent implements OnInit {
             `Are you sure that you want to exit ${this.course.description}?`
         );    
     }
+
+    
 }
 
 
